55b40e3225edc2eab79036efd535bc9eb4af9f05
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-05-21 16:38:02 -0300

    parte 1
    precisa dar permissão

---
+++ wallpaper.sh
@@ -0,0 +1,6 @@
+cd ~/'Área de Trabalho'/
+mkdir fotos
+cd ~/'Área de Trabalho'/fotos/
+wget https://wall.alphacoders.com/big.php?i=1363067.jpeg
+wget https://images8.alphacoders.com/136/1363075.jpeg
+
