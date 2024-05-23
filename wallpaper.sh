0d0af8f01b44949479ce1ed491755bc9fbea2027
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-05-23 16:24:51 -0300

    os links

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,9 @@
 cd ~/'Área de Trabalho'/
 mkdir fotos
 cd ~/'Área de Trabalho'/fotos/
-wget https://wall.alphacoders.com/big.php?i=1363067.jpeg
-wget https://images8.alphacoders.com/136/1363075.jpeg
-
+wget https://images3.alphacoders.com/136/thumb-1920-1363052.jpeg
+wget https://images6.alphacoders.com/136/thumb-1920-1363055.jpeg
+wget https://images5.alphacoders.com/136/thumb-1920-1363064.jpeg
+wget https://images7.alphacoders.com/136/thumb-1920-1363082.jpeg
+wget https://images4.alphacoders.com/136/thumb-1920-1363077.jpeg
+wget https://images4.alphacoders.com/136/thumb-1920-1363068.jpeg
