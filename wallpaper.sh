222bff1119e0af7bf3c689089245298982a91383
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-05-24 11:32:28 -0300

    mais fotos

--- wallpaper.sh
+++ wallpaper.sh
@@ -7,3 +7,7 @@ wget https://images5.alphacoders.com/136/thumb-1920-1363064.jpeg
 wget https://images7.alphacoders.com/136/thumb-1920-1363082.jpeg
 wget https://images4.alphacoders.com/136/thumb-1920-1363077.jpeg
 wget https://images4.alphacoders.com/136/thumb-1920-1363068.jpeg
+wget https://forza.azureedge.net/strapi-uploads/assets/massive_FH_5_Series_34_Reward_Cars_LAM_Countach_23_16x9_3840x2160_WM_441e88fe5b.webp
+wget https://forza.azureedge.net/strapi-uploads/assets/massive_FH_5_Series34_Hyundai_N_Vision74_04_16x9_3840x2160_WM_61a119c905.webp
+wget https://forza.azureedge.net/strapi-uploads/assets/massive_FH_5_Series_34_Reward_Cars_LOT_Esprit_Turbo_80_16x9_3840x2160_WM_da53616287.webp
+wget https://forza.azureedge.net/strapi-uploads/assets/massive_FH_5_Series_34_Reward_Cars_CIT_BX_4_TC_86_16x9_3840x2160_WM_bd0f75b16c.webp
