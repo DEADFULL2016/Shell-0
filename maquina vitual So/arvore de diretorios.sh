b97d3c49e3a3b974765f03bbe3e71d46be5f9ab1
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-06-16 13:16:43 -0300

    tem coisa pra fazer manual mente

--- maquina vitual So/arvore de diretorios.sh
+++ maquina vitual So/arvore de diretorios.sh
@@ -5,3 +5,11 @@ mkdir adm
 mkdir financeiro
 mkdir ti
 mkdir publico
+mkdir estagiarios
+
+
+sudo chmod -R 007 adm
+sudo chmod -R 007 financeiro
+sudo chmod -R 074 ti
+sudo chmod -R 706 publico
+sudo chmod -R 004 estagiarios
