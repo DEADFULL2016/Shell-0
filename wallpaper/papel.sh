e1c3e9e8f9e7a6a9e78d102ee2d42f8926c37d1d
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-08-28 14:41:04 -0300

    foi agr?

--- wallpaper/papel.sh
+++ wallpaper/papel.sh
@@ -2,7 +2,7 @@
 
 #colocar o wallpaper no desktop pegando a imagem que estiver na pasta aleatoriamente
 
-cd /home/anderson_2infor/Documentos/GitHub/Shell-0/wallpaper/fotos || exit
+cd pwd /fotos || exit
 
 
 foto=$(ls *.jpeg *.jpg *.webp | shuf -n1)
